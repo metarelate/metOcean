a25c17f8760027864b4b4f05af47851eac0b0763
marqh <markh@metarelate.net>
2015-01-04 09:42:37 +0000

    validation

--- lib/metarelate_metocean/validation/__init__.py
+++ lib/metarelate_metocean/validation/__init__.py
@@ -1,27 +1,32 @@
 import requests
 
+subformat_predicates = ['<http://codes.wmo.int/def/common/edition>']
 
-def _comp_fails(fuseki_process, comp_fails):
+def _comp_fails(fuseki_process, comp_fails, graph=None):
     """
     find all valid mappings which use acomp as a source or target
     """
+    graphs = 'FROM NAMED <http://metarelate.net/mappings.ttl>\n'
+    if graph:
+        graphs += 'FROM NAMED <http://metarelate.net/{}mappings.ttl>\n'.format(graph)
     val_errors = []
     for failure in comp_fails:
         qstr = ('SELECT ?amap \n'
+                '%(g)s'
                 'WHERE {\n'
-                'GRAPH <http://metarelate.net/mappings.ttl> {\n'
+                'GRAPH ?g {\n'
                 '{?amap mr:source %(c)s .}\n'
                 'UNION\n'
                 '{?amap mr:target %(c)s .}\n'
                 'MINUS {?amap ^dc:replaces+ ?anothermap}\n'
-                '}}' % {'c':failure.get('acomp')})
+                '}}' % {'c':failure.get('acomp'), 'g':graphs})
         results = fuseki_process.run_query(qstr)
         val_errors = val_errors + results
     return val_errors
 
     
 
-def cfunits(fuseki_process):
+def cfunits(fuseki_process, graph=None):
     """
     Validate that cf units rdfobject literals are 
     able to be parsed by udunits
@@ -42,14 +47,18 @@ def cfunits(fuseki_process):
     val_errors_response = {'CF units not parseable':val_errors}
     return val_errors_response
 
-# def longnamespace(fuseki_process):
+# def longnamespace(fuseki_process, graph=None):
 #     """
 #     """
+#     graphs = 'FROM NAMED <http://metarelate.net/concepts.ttl>\n'
+#     if graph:
+#         graphs += 'FROM NAMED <http://metarelate.net/{}concepts.ttl>\n'.format(graph)
 #     qstr = ('SELECT ?acomp ?long_name \n'
+#             '%s'
 #             'WHERE {\n'
-#             'GRAPH <http://metarelate.net/concepts.ttl> {\n'
+#             'GRAPH ?g {\n'
 #             '?acomp <http://def.scitools.org.uk/cfdatamodel/long_name> ?long_name'
-#             '}}\n')
+#             '}}\n' % graphs)
 #     results = fuseki_process.run_query(qstr)
 #     ufails = []
 #     for result in results:
@@ -59,20 +68,25 @@ def cfunits(fuseki_process):
 #     val_errors_response = {'Spaces in CF long_name':val_errors}
 #     return val_errors_response    
 
-def cflongnameisstd(fuseki_process):
+def cflongnameisstd(fuseki_process, graph=None):
+    # needs threading: slow
     """
     """
+    graphs = 'FROM NAMED <http://metarelate.net/concepts.ttl>\n'
+    if graph:
+        graphs += 'FROM NAMED <http://metarelate.net/{}concepts.ttl>\n'.format(graph)
     stdn = 'http://vocab.nerc.ac.uk/standard_name/{}/'
     val_errors = []
     qstr = ('SELECT ?acomp ?long_name \n'
+            '%s'
             'WHERE {\n'
-            'GRAPH <http://metarelate.net/concepts.ttl> {\n'
+            'GRAPH ?g {\n'
             '?acomp <http://def.scitools.org.uk/cfdatamodel/long_name> ?long_name'
-            '}}\n')
+            '}}\n' % graphs)
     results = fuseki_process.run_query(qstr)
     ufails = []
     for result in results:
-        std_url = stdn.format(result.get('long_name').strip('"'))
+        std_url = stdn.format(result.get('long_name').strip('"').strip())
         resp = requests.get(std_url)
         if resp.status_code == 200:
             ufails.append(result)
